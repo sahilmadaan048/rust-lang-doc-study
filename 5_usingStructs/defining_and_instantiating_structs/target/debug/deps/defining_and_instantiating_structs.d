620bb484d84053b1f703d45b1eab50f00368dd28
Sahil Madaan <madaan.sahil27@gmail.com>
2025-02-25 01:10:40 +0530

    done with the intro of structs

---
+++ 5_usingStructs/defining_and_instantiating_structs/target/debug/deps/defining_and_instantiating_structs.d
@@ -0,0 +1,5 @@
+E:\Code_Everyday\RUST\doc study\rust-lang-doc-study\5_usingStructsToStructureRelatedData\defining_and_instantiating_structs\target\debug\deps\defining_and_instantiating_structs.exe: src\main.rs
+
+E:\Code_Everyday\RUST\doc study\rust-lang-doc-study\5_usingStructsToStructureRelatedData\defining_and_instantiating_structs\target\debug\deps\defining_and_instantiating_structs.d: src\main.rs
+
+src\main.rs:
