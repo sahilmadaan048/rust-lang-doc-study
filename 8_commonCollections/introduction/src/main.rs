511808923b745f148e0fbdebc4ee08883a90afef
Sahil Madaan <madaan.sahil27@gmail.com>
2025-03-01 22:45:39 +0530

    modified files

--- 8_commonCollections/introduction/src/main.rs
+++ 8_commonCollections/introduction/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
-}
+what we will learn ?
+
+useful data strcutures oin rust stamndard library are called collections
+most data types have one specofoc value, but collections can contain 
+multiple values
+
+the data stored in the data types unlike array s and tuple types, is stored
+on the heap, which means the amount of data does not need tio be known at the
+compile time, and can grow or shrink s the program runs
+
+
+what collectiosn we will learn ?
+1. vector => same like c++
+2. string => collection of characters in depth we'll do 
+3. hashmap => mose particular implementatoin of the generak data structure called a map 
+
+create and update vectors, stroings and hash maps
