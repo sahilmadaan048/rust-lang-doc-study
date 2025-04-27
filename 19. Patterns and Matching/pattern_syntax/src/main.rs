75c37780a9c79066a729cb701bb58f56b631a2f5
Sahil Madaan <madaan.sahil27@gmail.com>
2025-04-28 00:04:32 +0530

    bindings done

--- 19. Patterns and Matching/pattern_syntax/src/main.rs
+++ 19. Patterns and Matching/pattern_syntax/src/main.rs
@@ -291,4 +291,30 @@ match x {
 }
 
 
-//i am also doing  this for my own good
+//BINDINGS
+// using @ operator
+
+enum Message {
+    Hello { id: i32 },
+}
+
+let msg = Message::Hello { id: 5 };
+
+match msg {
+    Message::Hello {
+        id: id_variable @ 3..=7,
+    } => println!("Found an id in range: {id_variable}"),
+    Message::Hello { id: 10..=12 } => {
+        println!("Found an id in another range")
+    }
+    Message::Hello { id } => println!("Found some other id: {id}"),
+}
+
+/*
+
+binding concept samjh nhi aa raha kya hai ye ?
+
+The at operator @ lets us create a variable that holds a value at 
+the same time as we’re testing that value for a pattern match
+
+*/
